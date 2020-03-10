404832a58f701ad2c0f053ef9cb70e11c93bee83
caihongquan <caihongquan@medlinker.com>
2020-03-10 17:08:06 +0800

    feat: init commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  title: 'Hello VuePress',
+  description: 'Just playing around',
+  themeConfig: {
+    nextLinks: false,
+    prevLinks: false,
+    smoothScroll: true,
+    nav: [
+      { text: 'Home', link: '/' },
+      { 
+        text: 'Demo',
+        items: [
+          { text: 'Demo1', link: '/demo1/' },
+          { text: 'Demo2', link: '/demo2/' },
+        ]
+      }
+    ],
+    sidebar: [
+      '/',
+      '/demo1/',
+      '/demo2/'
+    ]
+  }
+}
